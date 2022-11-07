a361bb00a16b7bcf95470db8d9f3afe66b9c51e0
ngrahovac <ninagrahovac.96@gmail.com>
2022-11-07 20:56:03 +0100

    feat: add logging to signup service method

--- src/edu4.Application/UsersService.cs
+++ src/edu4.Application/UsersService.cs
@@ -1,5 +1,6 @@
 using edu4.Application.External;
 using edu4.Domain.Users;
+using Microsoft.Extensions.Logging;
 
 namespace edu4.Application;
 
@@ -7,11 +8,16 @@ public class UsersService
 {
     private readonly IUsersRepository _users;
     private readonly IAccountManagementService _accountManagement;
+    private readonly ILogger<UsersService> _logger;
 
-    public UsersService(IUsersRepository users, IAccountManagementService accountManagement)
+    public UsersService(
+        IUsersRepository users,
+        IAccountManagementService accountManagement,
+        ILogger<UsersService> logger)
     {
         _users = users;
         _accountManagement = accountManagement;
+        _logger = logger;
     }
 
     /// <summary>
@@ -36,11 +42,14 @@ public class UsersService
 
         if (await _users.GetByAccountIdAsync(accountId) is not null)
         {
+            _logger.LogError("User with {AccountId} already signed in", accountId);
             throw new InvalidOperationException($"User with account id {accountId} already signed up");
         }
 
         await _users.AddAsync(user);
         await _accountManagement.MarkUserSignedUpAsync(accountId);  // Q: cross-process transaction?
+
+        _logger.LogInformation("User {User} successfully signed in", user);
 
         return user;
     }
