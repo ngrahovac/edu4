96584e0949c2748f1b9e7c92bca9590574a2948d
ngrahovac <ninagrahovac.96@gmail.com>
2022-11-12 17:38:30 +0100

    fix: disable nullable context in input model

--- src/edu4.API/Models/HatInputModel.cs
+++ src/edu4.API/Models/HatInputModel.cs
@@ -2,10 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace edu4.API.Models;
 
+#nullable disable
 public class HatInputModel
 {
     [Required]
-    public string? Type { get; set; }
+    public string Type { get; set; }
 
-    public Dictionary<string, object>? Parameters { get; set; }
+    public Dictionary<string, object> Parameters { get; set; }
 }
+#nullable restore
