8dd72ef9191621ecacf6dd84da928fd7a6f37d2d
Nikolina Grahovac <ninagrahovac.96@gmail.com>
2023-04-01 14:22:55 +0200

    test: expand and refactor projects service tests

--- tests/edu4.Application.Tests/ProjectsServiceTests.cs
+++ tests/edu4.Application.Tests/ProjectsServiceTests.cs
@@ -73,125 +73,9 @@ public class ProjectsServiceTests
         retrievedProject.Author.Should().Be(new Author(author.Id));
         retrievedProject.Positions.Count.Should().Be(positions.Count);
     }
-    #region remove
-    [Fact]
-    public async void Returns_all_projects_with_a_position_with_requirements_fit_for_a_student()
-    {
-        // ARRANGE
-        var studentHat = new StudentHat("Software Engineering", AcademicDegree.Masters);
-
-        var config = new ConfigurationBuilder().AddUserSecrets(GetType().Assembly)
-            .Build();
-
-        await new DbUtils(config).CleanDatabaseAsync();
-
-        await new ProjectFactory().WithPositions(
-            new List<Position>()
-            {
-                new PositionFactory().WithRequirements(
-                    HatFactory.OfType(HatType.Student)
-                    .WithStudyField("Software Engineering")
-                    .WithAcademicDegree(AcademicDegree.Bachelors)
-                    .Build())
-                .Build(),
-
-                new PositionFactory().WithRequirements(
-                    HatFactory.OfType(HatType.Student)
-                    .WithStudyField("Software Engineering")
-                    .WithAcademicDegree(AcademicDegree.Doctorate)
-                    .Build())
-                .Build()
-            })
-            .SeedAsync();
-
-        await new ProjectFactory().WithPositions(
-            new List<Position>()
-            {
-                new PositionFactory().WithRequirements(
-                    HatFactory.OfType(HatType.Student)
-                    .WithStudyField("Software Engineering")
-                    .WithAcademicDegree(AcademicDegree.Masters)
-                    .Build())
-                .Build()
-            })
-            .SeedAsync();
-
-        var sut = new ProjectsService(
-            new MongoDBProjectsRepository(config),
-            new MongoDbUsersRepository(config),
-            new NullLogger<ProjectsService>());
-
-        // ACT
-        var retrievedProjects = await sut.GetRecommendedForUserWearing(studentHat);
-
-        // ASSERT
-        retrievedProjects.Count.Should().Be(2);
-    }
 
     [Fact]
-    public async void Returns_all_projects_with_a_position_with_requirements_fit_for_an_academic()
-    {
-        // ARRANGE
-        var config = new ConfigurationBuilder()
-            .AddUserSecrets(GetType().Assembly)
-            .Build();
-
-        await new DbUtils(config).CleanDatabaseAsync();
-
-        var usersAcademicHat = new AcademicHat("Software Engineering");
-
-        await new ProjectFactory().WithPositions(
-            new List<Position>()
-            {
-                new PositionFactory().WithRequirements(
-                    HatFactory.OfType(HatType.Student)
-                    .WithStudyField("Software Engineering")
-                    .WithAcademicDegree(AcademicDegree.Bachelors)
-                    .Build())
-                .Build()
-            })
-            .SeedAsync();
-
-        await new ProjectFactory().WithPositions(
-            new List<Position>()
-            {
-                new PositionFactory().WithRequirements(
-                    HatFactory.OfType(HatType.Academic)
-                    .WithResearchField("Software Engineering")
-                    .Build())
-                .Build()
-            })
-            .SeedAsync();
-
-        await new ProjectFactory().WithPositions(
-           new List<Position>()
-           {
-                new PositionFactory().WithRequirements(
-                    HatFactory.OfType(HatType.Academic)
-                    .WithResearchField("Electronics Engineering")
-                    .Build())
-                .Build()
-           })
-           .SeedAsync();
-
-
-        var projects = new MongoDBProjectsRepository(config);
-        var users = new MongoDbUsersRepository(config);
-
-        var sut = new ProjectsService(
-            projects,
-            users,
-            new NullLogger<ProjectsService>());
-
-        // ACT
-        var retrievedProjects = await sut.GetRecommendedForUserWearing(usersAcademicHat);
-
-        // ASSERT
-        retrievedProjects.Count.Should().Be(1);
-    }
-    #endregion
-    [Fact]
-    public async void Discovering_projects_by_keyword_should_also_return_projects_with_the_keyword_in_title_only()
+    public async void Discovering_projects_by_keyword_should_include_projects_with_the_keyword_in_title_only()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -244,7 +128,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async void Discovering_projects_by_keyword_should_also_return_projects_with_the_keyword_in_description_only()
+    public async void Discovering_projects_by_keyword_should_includen_projects_with_the_keyword_in_description_only()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -297,7 +181,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async void Discovering_projects_by_keyword_should_also_return_projects_with_the_keyword_in_position_name_only()
+    public async void Discovering_projects_by_keyword_should_include_projects_with_the_keyword_in_position_name_only()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -347,7 +231,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async void Discovering_projects_by_keyword_should_also_return_projects_with_the_keyword_in_position_description_only()
+    public async void Discovering_projects_by_keyword_should_include_projects_with_the_keyword_in_position_description_only()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -397,7 +281,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async void Discovering_projects_by_keyword_should_return_projects_containing_the_keyword_in_title_or_description_or_position_name_or_description()
+    public async void Discovering_projects_by_keyword_should_return_projects_with_the_keyword_in_title_or_description_or_in_any_position_name_or_description()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -491,7 +375,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async Task Discovering_projects_by_omitting_the_keyword_should_return_all_projects()
+    public async Task Discovering_projects_without_specifying_search_refinements_should_return_all_projects()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -549,7 +433,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async Task Discovering_projects_by_requesting_them_to_be_sorted_from_oldest_should_return_all_projects_as_a_sorted_collection()
+    public async Task Discovering_projects_and_requesting_sorting_by_oldest_first_should_return_a_sorted_collection()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -605,7 +489,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async Task Discovering_projects_by_requesting_them_to_be_sorted_from_newest_should_return_all_projects_as_a_sorted_collection()
+    public async Task Discovering_projects_and_requesting_sorting_by_newest_first_should_return_a_sorted_collection()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -661,7 +545,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async Task Discovering_projects_by_keyword_and_requesting_them_to_be_sorted_from_oldest_posted_should_return_a_sorted_collection()
+    public async Task Discovering_projects_by_keyword_and_requesting_sorting_by_oldest_first_posted_should_return_projects_containing_the_keyword_as_a_sorted_collection()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -723,7 +607,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async Task Discovering_projects_by_keyword_and_requesting_them_to_be_sorted_from_newest_posted_should_return_a_sorted_collection()
+    public async Task Discovering_projects_by_keyword_and_requesting_sorting_by_newest_first_should_return_projects_containing_the_keyword_as_a_sorted_collection()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -966,7 +850,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async Task Discovering_projects_by_keyword_and_a_student_hat_returns_projects_with_min_one_position_with_requirements_that_the_hat_fits()
+    public async Task Discovering_projects_by_keyword_and_a_student_hat_returns_projects_containing_the_keyword_with_min_one_position_with_requirements_that_the_hat_fits()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -1277,7 +1161,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async Task Discovering_projects_by_specifying_users_academic_hat_and_requesting_they_be_sorted_from_oldest_posted_returns_projects_with_min_one_position_with_requirements_that_the_hat_fits_as_a_sorted_collection()
+    public async Task Discovering_projects_by_specifying_users_academic_hat_and_requesting_sorting_from_oldest_first_returns_projects_with_min_one_position_with_requirements_that_the_hat_fits_as_a_sorted_collection()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -1349,7 +1233,7 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async Task Discovering_projects_by_specifying_users_academic_hat_and_requesting_they_be_sorted_from_newest_posted_returns_projects_with_min_one_position_with_requirements_that_the_hat_fits_as_a_sorted_collection()
+    public async Task Discovering_projects_by_specifying_users_academic_hat_and_requesting_sorting_by_newest_first_returns_projects_with_min_one_position_with_requirements_that_the_hat_fits_as_a_sorted_collection()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
@@ -1421,7 +1305,157 @@ public class ProjectsServiceTests
     }
 
     [Fact]
-    public async Task Discovering_projects_by_keyword_and_specifying_users_academic_hat_and_requesting_they_be_sorted_from_newest_posted_returns_projects_with_min_one_position_with_requirements_that_the_hat_fits_as_a_sorted_collection()
+    public async Task Discovering_projects_by_specifying_users_student_hat_and_requesting_sorting_from_oldest_first_returns_projects_with_min_one_position_with_requirements_that_the_hat_fits_as_a_sorted_collection()
+    {
+        // ARRANGE
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets(GetType().Assembly)
+            .Build();
+
+        await new DbUtils(config).CleanDatabaseAsync();
+
+        var studentHat = new StudentHat("Computer Science", AcademicDegree.Masters);
+
+        await new ProjectFactory().WithPositions(
+            new List<Position>()
+            {
+                new PositionFactory().WithRequirements(
+                    HatFactory.OfType(HatType.Student)
+                    .WithStudyField("Computer Science")
+                    .WithAcademicDegree(AcademicDegree.Bachelors)
+                    .Build())
+                .Build()
+            })
+            .SeedAsync();
+
+        await new ProjectFactory().WithPositions(
+            new List<Position>()
+            {
+                new PositionFactory().WithRequirements(
+                    HatFactory.OfType(HatType.Academic)
+                    .WithResearchField("Computer Science")
+                    .Build())
+                .Build(),
+
+                new PositionFactory().WithRequirements(
+                    HatFactory.OfType(HatType.Student)
+                    .WithStudyField("Computer Science")
+                    .WithAcademicDegree(AcademicDegree.Masters)
+                    .Build())
+                .Build()
+            })
+            .SeedAsync();
+
+        await new ProjectFactory().WithPositions(
+            new List<Position>()
+            {
+                new PositionFactory().WithRequirements(
+                    HatFactory.OfType(HatType.Student)
+                    .WithAcademicDegree(AcademicDegree.Doctorate)
+                    .Build())
+                .Build()
+            })
+            .SeedAsync();
+
+        var projects = new MongoDBProjectsRepository(config);
+        var users = new MongoDbUsersRepository(config);
+
+        var sut = new ProjectsService(
+            projects,
+            users,
+            new NullLogger<ProjectsService>());
+
+        // ACT
+        var discoveredProjects = await sut.DiscoverAsync(null, ProjectsSortOption.OldestFirst, studentHat);
+
+        // ASSERT
+        discoveredProjects.Count.Should().Be(2);
+
+        foreach (var project in discoveredProjects)
+        {
+            project.Positions.Any(p => studentHat.Fits(p.Requirements)).Should().BeTrue();
+        }
+
+        discoveredProjects.Should().BeEquivalentTo(discoveredProjects.OrderBy(p => p.DatePosted), options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public async Task Discovering_projects_by_specifying_users_student_hat_and_requesting_sorting_by_newest_first_returns_projects_with_min_one_position_with_requirements_that_the_hat_fits_as_a_sorted_collection()
+    {
+        // ARRANGE
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets(GetType().Assembly)
+            .Build();
+
+        await new DbUtils(config).CleanDatabaseAsync();
+
+        var studentHat = new StudentHat("Computer Science", AcademicDegree.Masters);
+
+        await new ProjectFactory().WithPositions(
+            new List<Position>()
+            {
+                new PositionFactory().WithRequirements(
+                    HatFactory.OfType(HatType.Student)
+                    .WithStudyField("Computer Science")
+                    .WithAcademicDegree(AcademicDegree.Bachelors)
+                    .Build())
+                .Build()
+            })
+            .SeedAsync();
+
+        await new ProjectFactory().WithPositions(
+            new List<Position>()
+            {
+                new PositionFactory().WithRequirements(
+                    HatFactory.OfType(HatType.Academic)
+                    .WithResearchField("Computer Science")
+                    .Build())
+                .Build(),
+
+                new PositionFactory().WithRequirements(
+                    HatFactory.OfType(HatType.Student)
+                    .WithStudyField("Computer Science")
+                    .WithAcademicDegree(AcademicDegree.Masters)
+                    .Build())
+                .Build()
+            })
+            .SeedAsync();
+
+        await new ProjectFactory().WithPositions(
+            new List<Position>()
+            {
+                new PositionFactory().WithRequirements(
+                    HatFactory.OfType(HatType.Student)
+                    .WithAcademicDegree(AcademicDegree.Doctorate)
+                    .Build())
+                .Build()
+            })
+            .SeedAsync();
+
+        var projects = new MongoDBProjectsRepository(config);
+        var users = new MongoDbUsersRepository(config);
+
+        var sut = new ProjectsService(
+            projects,
+            users,
+            new NullLogger<ProjectsService>());
+
+        // ACT
+        var discoveredProjects = await sut.DiscoverAsync(null, ProjectsSortOption.NewestFirst, studentHat);
+
+        // ASSERT
+        discoveredProjects.Count.Should().Be(2);
+
+        foreach (var project in discoveredProjects)
+        {
+            project.Positions.Any(p => studentHat.Fits(p.Requirements)).Should().BeTrue();
+        }
+
+        discoveredProjects.Should().BeEquivalentTo(discoveredProjects.OrderByDescending(p => p.DatePosted), options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public async Task Discovering_projects_by_keyword_and_specifying_users_academic_hat_and_requesting_sorting_by_newest_first_returns_projects_containing_the_keyword_with_min_one_position_with_requirements_that_the_hat_fits_as_a_sorted_collection()
     {
         // ARRANGE
         var config = new ConfigurationBuilder()
