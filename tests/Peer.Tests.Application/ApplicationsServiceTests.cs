f6a7608bb20b12a8cd18b72313743de9383b5972
Nikolina Grahovac <ninagrahovac.96@gmail.com>
2023-05-26 17:44:29 +0200

    test: test service method for rejecting an application

--- tests/Peer.Tests.Application/ApplicationsServiceTests.cs
+++ tests/Peer.Tests.Application/ApplicationsServiceTests.cs
@@ -393,4 +393,147 @@ public class ApplicationsServiceTests
         // ASSERT
         await acceptingTheSameApplicationForTheSecondTime.Should().ThrowAsync<InvalidOperationException>();
     }
+
+    [Fact]
+    public async void Project_author_can_reject_a_submitted_application()
+    {
+        // ARRANGE
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets(GetType().Assembly)
+            .Build();
+
+        var applications = new MongoDbApplicationsRepository(config);
+        var sut = new ApplicationsService(
+            applications,
+            new MongoDbContributorsRepository(config),
+            new MongoDBProjectsRepository(config),
+            new NullLogger<ApplicationsService>());
+
+        var author = await new ContributorFactory().SeedAsync();
+
+        var project = await new ProjectFactory()
+           .WithAuthorId(author.Id)
+           .WithPositions(new List<Position>() { new PositionFactory().Build() })
+           .SeedAsync();
+
+        var application = await new ApplicationsFactory()
+            .WithProjectId(project.Id)
+            .WithPositionId(project.Positions.ElementAt(0).Id)
+            .SeedAsync();
+
+        // ACT
+        await sut.RejectAsync(project.AuthorId, application.Id);
+
+        // ASSERT
+        var retrievedApplication = await applications.GetByIdAsync(application.Id);
+        retrievedApplication.Status.Should().Be(ApplicationStatus.Rejected);
+    }
+
+    [Fact]
+    public async void Application_cannot_be_rejected_by_a_contributor_that_is_not_the_project_author()
+    {
+        // ARRANGE
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets(GetType().Assembly)
+            .Build();
+
+        var applications = new MongoDbApplicationsRepository(config);
+        var sut = new ApplicationsService(
+            applications,
+            new MongoDbContributorsRepository(config),
+            new MongoDBProjectsRepository(config),
+            new NullLogger<ApplicationsService>());
+
+        var author = await new ContributorFactory().SeedAsync();
+
+        var project = await new ProjectFactory()
+           .WithAuthorId(author.Id)
+           .WithPositions(new List<Position>() { new PositionFactory().Build() })
+           .SeedAsync();
+
+        var requester = await new ContributorFactory().SeedAsync();
+
+        var application = await new ApplicationsFactory()
+            .WithProjectId(project.Id)
+            .WithPositionId(project.Positions.ElementAt(0).Id)
+            .SeedAsync();
+
+        // ACT
+        var rejectingApplicationForAProjectAuthoredByAnother = async () => await sut.RejectAsync(requester.Id, application.Id);
+
+        // ASSERT
+        await rejectingApplicationForAProjectAuthoredByAnother.Should().ThrowAsync<InvalidOperationException>();
+    }
+
+    [Fact]
+    public async void Project_author_cannot_reject_the_same_application_twice()
+    {
+        // ARRANGE
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets(GetType().Assembly)
+            .Build();
+
+        var applications = new MongoDbApplicationsRepository(config);
+        var sut = new ApplicationsService(
+            applications,
+            new MongoDbContributorsRepository(config),
+            new MongoDBProjectsRepository(config),
+            new NullLogger<ApplicationsService>());
+
+        var author = await new ContributorFactory().SeedAsync();
+
+        var project = await new ProjectFactory()
+           .WithAuthorId(author.Id)
+           .WithPositions(new List<Position>() { new PositionFactory().Build() })
+           .SeedAsync();
+
+        var application = await new ApplicationsFactory()
+            .WithProjectId(project.Id)
+            .WithPositionId(project.Positions.ElementAt(0).Id)
+            .SeedAsync();
+
+        await sut.RejectAsync(project.AuthorId, application.Id);
+
+        // ACT
+        var rejectingTheSameApplicationTwice = async () => await sut.RejectAsync(project.AuthorId, application.Id);
+
+        // ASSERT
+        await rejectingTheSameApplicationTwice.Should().ThrowAsync<InvalidOperationException>();
+    }
+
+    [Fact]
+    public async void Project_author_cannot_reject_an_accepted_application()
+    {
+        // ARRANGE
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets(GetType().Assembly)
+            .Build();
+
+        var applications = new MongoDbApplicationsRepository(config);
+        var sut = new ApplicationsService(
+            applications,
+            new MongoDbContributorsRepository(config),
+            new MongoDBProjectsRepository(config),
+            new NullLogger<ApplicationsService>());
+
+        var author = await new ContributorFactory().SeedAsync();
+
+        var project = await new ProjectFactory()
+           .WithAuthorId(author.Id)
+           .WithPositions(new List<Position>() { new PositionFactory().Build() })
+           .SeedAsync();
+
+        var application = await new ApplicationsFactory()
+            .WithProjectId(project.Id)
+            .WithPositionId(project.Positions.ElementAt(0).Id)
+            .SeedAsync();
+
+        await sut.AcceptAsync(author.Id, application.Id);
+
+        // ACT
+        var rejectingAnAcceptedApplication = async () => await sut.RejectAsync(project.AuthorId, application.Id);
+
+        // ASSERT
+        await rejectingAnAcceptedApplication.Should().ThrowAsync<InvalidOperationException>();
+    }
 }
