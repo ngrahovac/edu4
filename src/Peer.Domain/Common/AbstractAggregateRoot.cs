452900ac1163c8acd6ea766df8443475fbcd443c
Nikolina Grahovac <ninagrahovac.96@gmail.com>
2023-07-04 10:19:10 +0200

    feat: add raising domain events from ARs

--- src/Peer.Domain/Common/AbstractAggregateRoot.cs
+++ src/Peer.Domain/Common/AbstractAggregateRoot.cs
@@ -2,4 +2,11 @@ namespace Peer.Domain.Common;
 
 public class AbstractAggregateRoot : AbstractEntity
 {
+    private readonly List<AbstractDomainEvent> _domainEvents = new();
+    public IReadOnlyList<AbstractDomainEvent> DomainEvents => _domainEvents;
+
+    protected void RaiseDomainEvent(AbstractDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
 }
