8db4d9c2a5f72c87cf2c7743b252528016f5dd75
Nina <ninagrahovac.96@gmail.com>
2023-07-09 17:25:58 +0200

    feat: notify applicant that their application was accepted

--- src/Peer.Application/Handlers/ApplicationAcceptedHandler.cs
+++ src/Peer.Application/Handlers/ApplicationAcceptedHandler.cs
@@ -1,16 +1,26 @@
 using Peer.Application.Contracts;
 using Peer.Domain.Collaborations;
+using Peer.Domain.Contributors;
+using Peer.Domain.Notifications;
 
 namespace Peer.Application.Handlers;
 public class ApplicationAcceptedHandler
 {
     private readonly IApplicationsRepository _applications;
     private readonly ICollaborationsRepository _collaborations;
+    private readonly IContributorsRepository _contributors;
+    private readonly INotificationsRepository _notifications;
 
-    public ApplicationAcceptedHandler(IApplicationsRepository applications, ICollaborationsRepository collaborations)
+    public ApplicationAcceptedHandler(
+        IApplicationsRepository applications,
+        ICollaborationsRepository collaborations,
+        IContributorsRepository contributors,
+        INotificationsRepository notifications)
     {
         _applications = applications;
         _collaborations = collaborations;
+        _contributors = contributors;
+        _notifications = notifications;
     }
 
     public async Task MakeApplicantACollaboratorOnTheProjectAsync(Guid applicationId)
@@ -23,5 +33,11 @@ public class ApplicationAcceptedHandler
             acceptedApplication.PositionId);
 
         await _collaborations.AddAsync(collaboration);
+
+
+        var applicant = await _contributors.GetByIdAsync(acceptedApplication.ApplicantId);
+        var notification = new OwnApplicationAccepted(applicant, acceptedApplication);
+
+        await _notifications.AddAsync(notification);
     }
 }
