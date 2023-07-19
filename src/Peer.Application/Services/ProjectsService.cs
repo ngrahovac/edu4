f751d4436115620fa88ce43c15c8d57e4a9fd83f
Nina <ninagrahovac.96@gmail.com>
2023-07-19 14:04:59 +0200

    fix: remove unnecessary param assignments

--- src/Peer.Application/Services/ProjectsService.cs
+++ src/Peer.Application/Services/ProjectsService.cs
@@ -60,9 +60,9 @@ public class ProjectsService
     }
 
     public async Task<IReadOnlyList<Project>> DiscoverAsync(
-        string? keyword = null,
-        ProjectsSortOption sortOption = ProjectsSortOption.Unspecified,
-        Hat? usersHat = null)
+        string? keyword,
+        ProjectsSortOption sortOption,
+        Hat? usersHat)
     {
         var discoveredProjects = await _projects.DiscoverAsync(keyword, sortOption, usersHat);
 
