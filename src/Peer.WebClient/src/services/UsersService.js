0f0f43f5960b6cc000e28b2e4b96037b08de242a
Nina <ninagrahovac.96@gmail.com>
2023-07-22 23:04:32 +0200

    feat: add service method for fetching a contributor

--- src/Peer.WebClient/src/services/UsersService.js
+++ src/Peer.WebClient/src/services/UsersService.js
@@ -35,4 +35,34 @@ async function me(accessToken) {
     }
 }
 
-export { me }
+async function getContributor(accessToken, contributorUrl) {
+    try {
+        const apiRootUri = process.env.REACT_APP_EDU4_API_ROOT_URI;
+
+        var response = await getAsync(`${apiRootUri}${contributorUrl}`, accessToken);
+
+        if (response.ok) {
+            var body = await response.json();
+
+            return {
+                outcome: successResult,
+                message: "Successfully fetched the current user!",
+                payload: body
+            };
+        } else {
+            var responseMessage = await response.text();
+
+            return {
+                outcome: failureResult,
+                message: responseMessage
+            };
+        }
+    } catch (error) {
+        return {
+            outcome: errorResult,
+            message: "The request failed. Please check your connection and try again."
+        };
+    }
+}
+
+export { me, getContributor }
