dda86f3378b15bbe3d0f78faebd707b66f25479f
Nina <ninagrahovac.96@gmail.com>
2023-07-22 22:26:44 +0200

    fix: update linking from top navbar item

--- src/Peer.WebClient/src/comps/nav/TopNavbarItem.js
+++ src/Peer.WebClient/src/comps/nav/TopNavbarItem.js
@@ -5,25 +5,25 @@ const TopNavbarItem = (props) => {
     const { text, icon, link } = props;
 
     return (
-        <div className="flex shrink-0">
-            <button className="flex items-center text-slate-700 font-semibold pl-4 pr-6 py-2 hover:rounded-full">
-                {
-                    text != false &&
-                    <div className='mr-2'>
-                        {icon}
-                    </div>
-                }
-                {
-                    text == false &&
-                    <div>
-                        {icon}
-                    </div>
-                }
-                <Link to={link}>
+        <Link to={link}>
+            <div className="flex shrink-0">
+                <button className="flex items-center text-slate-700 font-semibold pl-4 pr-6 py-2 hover:rounded-full">
+                    {
+                        text != false &&
+                        <div className='mr-2'>
+                            {icon}
+                        </div>
+                    }
+                    {
+                        text == false &&
+                        <div>
+                            {icon}
+                        </div>
+                    }
                     {text}
-                </Link>
-            </button>
-        </div >
+                </button>
+            </div >
+        </Link>
     )
 }
 
