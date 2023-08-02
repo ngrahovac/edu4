384aef9e4c496d24ef55df8b762e5ce54653039c
Nina <ninagrahovac.96@gmail.com>
2023-08-02 18:21:29 +0200

    feat: include contributor id in display model

--- src/Peer.API/Models/Display/ContributorDisplayModel.cs
+++ src/Peer.API/Models/Display/ContributorDisplayModel.cs
@@ -5,6 +5,7 @@ namespace Peer.API.Models.Display;
 #nullable disable
 public class ContributorDisplayModel
 {
+    public Guid Id { get; set; }
     public string FullName { get; set; }
     public string Email { get; set; }
     public IReadOnlyCollection<HatDisplayModel> Hats { get; set; }
@@ -12,6 +13,7 @@ public class ContributorDisplayModel
 
     public ContributorDisplayModel(Contributor user, bool isMe)
     {
+        Id = user.Id;
         FullName = user.FullName;
         Email = user.ContactEmail;
         Hats = user.Hats.Select(h => new HatDisplayModel(h)).ToList();
