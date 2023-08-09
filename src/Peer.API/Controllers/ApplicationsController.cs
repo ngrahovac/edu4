184a05b84e3fc3f1bd7b1cfba29ddc075d8d918e
Nina <ninagrahovac.96@gmail.com>
2023-08-09 13:00:12 +0200

    fix: update accepting application model binding

--- src/Peer.API/Controllers/ApplicationsController.cs
+++ src/Peer.API/Controllers/ApplicationsController.cs
@@ -85,12 +85,12 @@ public class ApplicationsController : ControllerBase
     }
 
     [HttpPut("{applicationId}")]
-    public async Task<ActionResult> AcceptAsync(Guid applicationId, [FromBody] ApplicationStatus status)
+    public async Task<ActionResult> AcceptAsync(Guid applicationId, [FromBody] AcceptingApplicationInputModel model)
     {
         var requesterAccountId = _accountIdExtractionService.ExtractAccountIdFromHttpRequest(Request);
         var requesterId = await _contributors.GetUserIdFromAccountId(requesterAccountId);
 
-        if (status is not ApplicationStatus.Accepted)
+        if (model.Status is not ApplicationStatus.Accepted)
         {
             return BadRequest("This endpoint is used only for accepting an application");
         }
