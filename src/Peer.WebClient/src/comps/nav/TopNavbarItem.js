bd3a58c3f5dcb824936726fe1432f61fc2f8efa0
Nina <ninagrahovac.96@gmail.com>
2023-08-22 00:16:11 +0200

    refactor: refactor top navbar item comp

--- src/Peer.WebClient/src/comps/nav/TopNavbarItem.js
+++ src/Peer.WebClient/src/comps/nav/TopNavbarItem.js
@@ -2,25 +2,28 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 const TopNavbarItem = (props) => {
-    const { text, icon, link } = props;
+    const {
+        text,
+        icon,
+        link
+    } = props;
 
     return (
         <Link to={link}>
             <div className="flex shrink-0">
-                <button className="flex items-center text-slate-700 font-semibold pl-4 pr-6 py-2 hover:rounded-full">
+                <button className="flex items-center text-slate-700 font-semibold pl-4 pr-6 py-2">
                     {
-                        text != false &&
-                        <div className='mr-2'>
+                        icon &&
+                        <div className={`${text ? 'mr-2' : ''}`}>
                             {icon}
                         </div>
                     }
                     {
-                        text == false &&
-                        <div>
-                            {icon}
-                        </div>
+                        text &&
+                        <>
+                            {text}
+                        </>
                     }
-                    {text}
                 </button>
             </div >
         </Link>
