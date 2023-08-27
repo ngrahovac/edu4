720b9b989d3058cce94ca8886e714695bbd27168
Nina <ninagrahovac.96@gmail.com>
2023-08-28 00:11:01 +0200

    fix: explicitly pass arg to handler prop

--- src/Peer.WebClient/src/comps/publish/AddedPosition.js
+++ src/Peer.WebClient/src/comps/publish/AddedPosition.js
@@ -10,7 +10,7 @@ const AddedPosition = (props) => {
     return (
         <div className='relative'>
             <div
-                onClick={onRemoved}
+                onClick={() => onRemoved(position)}
                 className='absolute top-2 right-2'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4 hover:stroke-red-700">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
