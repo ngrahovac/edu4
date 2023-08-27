4d2ef5e8da8f345871b020b2b11c3906204c4de9
Nina <ninagrahovac.96@gmail.com>
2023-08-27 23:05:36 +0200

    refactor: remove import

--- src/Peer.WebClient/src/comps/publish/BasicInfoForm.js
+++ src/Peer.WebClient/src/comps/publish/BasicInfoForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import InvalidFormFieldWarning from './InvalidFormFieldWarning';
 
 const BasicInfoForm = (props) => {
