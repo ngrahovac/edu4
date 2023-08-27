83a983b6fdc818aca41e52f961d3f19243cf5076
Nina <ninagrahovac.96@gmail.com>
2023-08-28 00:46:59 +0200

    feat: update style

--- src/Peer.WebClient/src/comps/publish/AddedPosition.js
+++ src/Peer.WebClient/src/comps/publish/AddedPosition.js
@@ -12,7 +12,7 @@ const AddedPosition = (props) => {
             <div
                 onClick={() => onRemoved(position)}
                 className='absolute top-2 right-2'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4 hover:stroke-red-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6 hover:stroke-red-700">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </div>
