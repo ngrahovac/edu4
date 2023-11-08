1171a4c81106c4ef6165178b6f8f7b725ad4ac6d
ngrahovac <ninagrahovac.96@gmail.com>
2023-11-08 21:47:10 +0100

    Add timestamp to domain events. Add ctor for ORM only.

--- src/Peer.Domain/Projects/ProjectRemoved.cs
+++ src/Peer.Domain/Projects/ProjectRemoved.cs
@@ -1,9 +1,15 @@
 using Peer.Domain.Common;
 
 namespace Peer.Domain.Projects;
+
 public class ProjectRemoved : AbstractDomainEvent
 {
     public Guid ProjectId { get; }
 
-    public ProjectRemoved(Project project) => ProjectId = project.Id;
+    [Obsolete("Used by ORM only")]
+    public ProjectRemoved(Guid projectId, DateTime timeRaised)
+        : base(timeRaised) => ProjectId = projectId;
+
+    public ProjectRemoved(Project project)
+        : base(DateTime.UtcNow) => ProjectId = project.Id;
 }
