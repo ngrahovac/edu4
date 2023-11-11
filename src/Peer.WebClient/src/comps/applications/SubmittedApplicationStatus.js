2da299405150757e59e7b30f511d8eb25dd356e2
ngrahovac <ninagrahovac.96@gmail.com>
2023-11-11 23:04:50 +0100

    Update submitted status flair style and prevent shrinking

--- src/Peer.WebClient/src/comps/applications/SubmittedApplicationStatus.js
+++ src/Peer.WebClient/src/comps/applications/SubmittedApplicationStatus.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SubmittedApplicationStatus = () => {
 
     return (
-        <div className='border border-1 rounded-lg p-1 flex place-content-center uppercase tracking-widest text-sm text-gray-700'>
+        <div className='border border-1 rounded-lg p-1 flex shrink-0 place-content-center uppercase tracking-widest text-xs font-medium bg-white border-slate-500 text-slate-500'>
             Submitted
         </div>
     )
