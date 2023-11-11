b5194aa2eff805507ca559d8ed0283a73a85e043
ngrahovac <ninagrahovac.96@gmail.com>
2023-11-11 23:36:41 +0100

    Update table comp style

--- src/Peer.WebClient/src/comps/shared/table/Table.js
+++ src/Peer.WebClient/src/comps/shared/table/Table.js
@@ -12,9 +12,9 @@ const Table = (props) => {
     let numberedColumns = columns.map(c => ({ id: index++, column: c })); // generating keys during rendering?
 
     return (
-        <table className='text-left table-auto w-full overflow-x-scroll'>
+        <table className='text-left table-auto w-full overflow-x-scroll border-collapse'>
             <thead>
-                <tr className='border-b border-gray-400'>
+                <tr className='border-b-2 border-gray-200'>
                     {
                         numberedColumns.map(c => <th key={c.id} className={`${widths[c.column] ?? ''} p-4`}>
                             {c.column}
@@ -25,11 +25,14 @@ const Table = (props) => {
             <tbody>
                 {children}
             </tbody>
-            <tfoot className=' border-gray-300'>
+            <tfoot className='border-t-2 border-gray-200'>
                 <tr>
-                    <td className='text-left p-4 h-12 uppercase tracking-wide'>
+                    <td className='text-left p-4 h-12 uppercase tracking-wider text-slate-500 text-sm font-medium'>
                         <p>{`Selected: ${selectedCount}`}</p>
                     </td>
+                    {
+                        columns.slice(1).map(c => <td></td>)
+                    }
                 </tr>
             </tfoot>
         </table>
