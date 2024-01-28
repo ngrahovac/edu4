1682729c87027c95bbf0bad09459a0516e0acb2b
ngrahovac <ninagrahovac.96@gmail.com>
2024-01-28 15:22:41 +0100

    Add button for deleting project

--- src/Peer.WebClient/src/pages/Project.js
+++ src/Peer.WebClient/src/pages/Project.js
@@ -18,6 +18,7 @@ import ConfirmationDialog from '../comps/shared/ConfirmationDialog';
 import SubsectionTitle from '../layout/SubsectionTitle';
 import PositionCardWithAuthorOptions from '../comps/project/PositionCardWithAuthorOptions';
 import PositionCardWithCollaboratorOptions from '../comps/project/PositionCardWithCollaboratorOptions';
+import DangerTertiaryButton from '../comps/buttons/DangerTertiaryButton';
 
 const Project = () => {
     const { projectId } = useParams();
@@ -33,6 +34,7 @@ const Project = () => {
     const submitApplicationConfirmationDialogRef = useRef(null);
     const revokeApplicationConfirmationDialogRef = useRef(null);
     const removePositionConfirmationDialogRef = useRef(null);
+    const deleteProjectConfirmationDialogRef = useRef(null);
 
     useEffect(() => {
         const fetchProject = () => {
@@ -206,6 +208,35 @@ const Project = () => {
         })();
     }
 
+    function handleDeleteProjectRequested() {
+        deleteProjectConfirmationDialogRef.current.showModal();
+    }
+
+    function handleDeleteProject() {
+        (async () => {
+            try {
+                let token = await getAccessTokenSilently({
+                    audience: process.env.REACT_APP_EDU4_API_IDENTIFIER
+                });
+
+                let result = await remove(project.id, token);
+
+                if (result.outcome === successResult) {
+                    console.log("success");
+                    setFetchUpdatedDataSwitch(!fetchUpdatedDataSwitch);
+                    setSelectedPosition(undefined);
+                    removePositionConfirmationDialogRef.current.close();
+                } else if (result.outcome === failureResult) {
+                    console.log("failure");
+                } else if (result.outcome === errorResult) {
+                    console.log("error");
+                }
+            } catch (ex) {
+                console.log("exception", ex);
+            }
+        })();
+    }
+
     if (pageLoading) {
         return (
             <SpinnerLayout>
@@ -353,13 +384,22 @@ const Project = () => {
                 </ConfirmationDialog>
             </dialog>
 
+            <dialog ref={deleteProjectConfirmationDialogRef}>
+                <ConfirmationDialog
+                    question="Delete project"
+                    description="You cannot undo this action"
+                    onCancel={() => deleteProjectConfirmationDialogRef.current.close()}
+                    onConfirm={handleDeleteProject}>
+                </ConfirmationDialog>
+            </dialog>
+
+
             {
                 project &&
                 <SingleColumnLayout
                     title={project.title}>
 
                     <div className='flex flex-col gap-y-8'>
-
                         <div className='flex flex-col gap-y-4'>
                             <SubsectionTitle title="Description"></SubsectionTitle>
                             <p className='text-gray-600'>{project.description}</p>
@@ -406,6 +446,20 @@ const Project = () => {
                                 </p>
                             }
                         </div>
+
+                        {
+                            project.authored &&
+                            <div className='flex flex-row-reverse py-16'>
+                                <DangerTertiaryButton
+                                    text="Delete project"
+                                    icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+                                    }
+                                    onClick={handleDeleteProjectRequested}>
+                                </DangerTertiaryButton>
+                            </div>
+                        }
                     </div>
                 </SingleColumnLayout>
             }
