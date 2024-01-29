38d68384ee5b68043820e28506b0133d6a31ad7b
ngrahovac <ninagrahovac.96@gmail.com>
2024-01-29 12:56:11 +0100

    Add search results page and navigate to it from global search

--- src/Peer.WebClient/src/comps/nav/GlobalSearch.js
+++ src/Peer.WebClient/src/comps/nav/GlobalSearch.js
@@ -1,36 +1,45 @@
 import React from 'react'
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const GlobalSearch = () => {
-
     const [keyword, setKeyword] = useState('');
     const [isTyping, setIsTyping] = useState(false);
+
+    const navigate = useNavigate();
 
     function handleKeywordChange(e) {
         setKeyword(e.target.value);
     }
 
+    function handleKeyDown(e) {
+        if (e.key == "Enter") {
+            navigate(`/search?keyword=${keyword}`)
+        }
+    }
+
     return (
         <div className='flex items-center justify-between bg-white px-4 gap-x-2 rounded-full'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="lightGray" className="w-5 h-5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="lightGray" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
 
-                <input
-                    type="text"
-                    name="keyword"
-                    placeholder="Search projects..."
-                    onChange={handleKeywordChange}
-                    maxLength={100}
-                    value={keyword}
-                    onFocus={() => setIsTyping(true)}
-                    onBlur={() => setIsTyping(false)}
-                    className="rounded-full border-none focus:border-none focus:ring-0 ">
-                </input>
+            <input
+                type="text"
+                name="keyword"
+                placeholder="Search projects..."
+                onChange={handleKeywordChange}
+                maxLength={100}
+                value={keyword}
+                onFocus={() => setIsTyping(true)}
+                onBlur={() => setIsTyping(false)}
+                onKeyDown={handleKeyDown}
+                className="rounded-full border-none focus:border-none focus:ring-0">
+            </input>
 
 
-                <span className={`text-gray-300 font-semibold ${isTyping ? '' : 'invisible'}`}>Enter</span>
-            </div>
+            <span className={`text-gray-300 font-semibold ${isTyping ? '' : 'invisible'}`}>Enter</span>
+        </div>
     )
 }
 
---
+++ src/Peer.WebClient/src/pages/Search.js
@@ -0,0 +1,130 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import SpinnerLayout from '../layout/SpinnerLayout';
+import { BeatLoader } from 'react-spinners';
+import { useAuth0 } from '@auth0/auth0-react';
+import ProjectCard from '../comps/discover/ProjectCard';
+import SingleColumnLayout from '../layout/SingleColumnLayout';
+import { me } from '../services/UsersService';
+import { successResult, failureResult, errorResult } from '../services/RequestResult';
+import { discover } from '../services/ProjectsService';
+
+const Search = () => {
+
+    const [search, setSearch] = useSearchParams();
+    const [keyword, setKeyword] = useState(undefined);
+    const [sort, setSort] = useState(undefined);
+    const [hat, setHat] = useState(undefined);
+
+    const [ownHats, setOwnHats] = useState(undefined);
+    const [projects, setProjects] = useState(undefined);
+
+    const [pageLoading, setPageLoading] = useState(true);
+    const [projectsLoading, setProjectsLoading] = useState(true);
+
+    const { getAccessTokenSilently } = useAuth0();
+
+    useEffect(() => {
+        setKeyword(search.get('keyword'));
+        setSort(search.get('sort'));
+        setHat(search.get('hat'));
+    }, [search])
+
+    useEffect(() => {
+        const fetchOwnHats = () => {
+            (async () => {
+                setPageLoading(true);
+
+                try {
+                    let token = await getAccessTokenSilently({
+                        audience: process.env.REACT_APP_EDU4_API_IDENTIFIER
+                    });
+
+                    let result = await me(token);
+                    setPageLoading(false);
+
+                    if (result.outcome === successResult) {
+                        setOwnHats(result.payload.hats);
+                    } else {
+                        console.log("error fetching users hats");
+                    }
+                } catch (ex) {
+                    console.log(ex);
+                } finally {
+                    setPageLoading(false);
+                }
+            })();
+        }
+
+        fetchOwnHats();
+    }, [getAccessTokenSilently])
+
+    useEffect(() => {
+        const handleDiscoveryRefinementsChange = () => {
+            (async () => {
+                try {
+                    setProjectsLoading(true);
+
+                    let token = await getAccessTokenSilently({
+                        audience: process.env.REACT_APP_EDU4_API_IDENTIFIER
+                    });
+
+                    let result = await discover(keyword, sort, hat, token);
+                    setProjectsLoading(false);
+
+                    if (result.outcome === successResult) {
+                        var projects = result.payload;
+                        setProjects(projects);
+                    } else if (result.outcome === failureResult) {
+                        console.log("failure");
+                    } else if (result.outcome === errorResult) {
+                        console.log("error");
+                    }
+                } catch (ex) {
+                    console.log("exception", ex);
+                } finally {
+                    setProjectsLoading(false);
+                }
+            })();
+        }
+
+        handleDiscoveryRefinementsChange();
+    }, [keyword, sort, hat])
+
+    if (pageLoading) {
+        return (
+            <SpinnerLayout>
+                <BeatLoader
+                    loading={pageLoading}
+                    size={24}
+                    color="blue">
+                </BeatLoader>
+            </SpinnerLayout>
+        )
+    }
+
+    const searchResults = projectsLoading ?
+        <BeatLoader></BeatLoader> :
+        projects && projects.length ?
+            <div className='flex flex-col space-y-8'>
+                {
+                    projects.map((p, index) => <div key={index}>
+                        <ProjectCard project={p}></ProjectCard>
+                    </div>)
+                }
+            </div> :
+            <p>There are currently no projects satisfying the criteria.</p>;
+
+    return (
+        ownHats &&
+        <SingleColumnLayout
+            title={`Search results for ${keyword}`}>
+            <div className='flex flex-col gap-y-8'>
+                {searchResults}
+            </div>
+        </SingleColumnLayout>
+    )
+}
+
+export default Search
