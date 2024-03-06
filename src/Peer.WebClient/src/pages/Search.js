8b0363f80737ae254fe741d8cb5b70f6cee7980a
ngrahovac <ninagrahovac.96@gmail.com>
2024-03-06 12:02:11 +0100

    Fix focus outline on global search

--- src/Peer.WebClient/src/pages/Search.js
+++ src/Peer.WebClient/src/pages/Search.js
@@ -230,13 +230,13 @@ const Search = () => {
             title="Discover projects"
             description="Search projects and find opportunities to contribute to bla bla">
             <div className='flex flex-col gap-y-16 relative'>
-                <div className='relative flex drop-shadow-md rounded-full bg-white px-6 py-4 gap-x-4 items-center mx-auto w-1/2'>
+                <div className='relative flex drop-shadow-md rounded-full bg-white px-6 py-4 gap-x-4 items-center mx-auto w-1/2 border border-transparent focus-within:border focus-within:border-indigo-500 focus-within:ring-indigo-500 focus-within:ring-1'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="lightgray" className="w-6 h-6 absolute left-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
                     <input
                         ref={pageTopRef}
-                        className='text-center w-full'
+                        className='text-center outline-none w-full'
                         value={keywordTyped}
                         onChange={e => setKeywordTyped(e.target.value)}>
                     </input>
