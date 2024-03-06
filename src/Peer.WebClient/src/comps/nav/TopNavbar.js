addb3af0f63198e974f6dd20b95542535a94bbcc
ngrahovac <ninagrahovac.96@gmail.com>
2024-03-06 20:30:28 +0100

    Add publish navbar item

--- src/Peer.WebClient/src/comps/nav/TopNavbar.js
+++ src/Peer.WebClient/src/comps/nav/TopNavbar.js
@@ -20,11 +20,11 @@ const TopNavbar = () => {
     }
 
     const topNavbarContextMenu = (
-    <div className='w-64 fixed top-14 right-8 z-50'>
-        <TopNavbarContextMenu
-            hidden={topNavbarContextMenuHidden}>
-        </TopNavbarContextMenu>
-    </div>);
+        <div className='w-64 fixed top-14 right-8 z-50'>
+            <TopNavbarContextMenu
+                hidden={topNavbarContextMenuHidden}>
+            </TopNavbarContextMenu>
+        </div>);
 
     return (isAuthenticated &&
         <>
@@ -37,7 +37,19 @@ const TopNavbar = () => {
                 </div>
 
                 {/* navbar items on the right */}
-                
+                <div className='flex items-center gap-x-4'>
+                    <Link to="/publish">
+                    <BorderlessButton
+                        text="Publish"
+                        icon={<>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                        </>}>
+                    </BorderlessButton>
+                    </Link>
+                </div>
+
             </div>
 
             <Outlet></Outlet>
