9a0ca7a3945ccd4a379324b86567d78accf01c73
ngrahovac <ninagrahovac.96@gmail.com>
2024-03-20 13:51:17 +0100

    Fetch previous page of sent applications

--- src/Peer.WebClient/src/comps/applications/SentApplications.js
+++ src/Peer.WebClient/src/comps/applications/SentApplications.js
@@ -241,10 +241,13 @@ const SentApplications = (props) => {
 
                             <ApplicationsTable.Footer.Cell collspan={3}>
                                 <div className='flex shrink-0 items-center w-full align-middle justify-center'>
-                                    <BorderlessButton icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                                    </svg>
-                                    }>
+                                    <BorderlessButton
+                                        onClick={() => { if (displayedApplicationsPage.previousPage != null) onPageChanged(displayedApplicationsPage.previousPage) }}
+                                        disabled={displayedApplicationsPage.previousPage == null}
+                                        icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                                        </svg>
+                                        }>
                                     </BorderlessButton>
 
                                     <p>
