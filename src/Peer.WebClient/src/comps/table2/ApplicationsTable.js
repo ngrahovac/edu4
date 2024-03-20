2f7be9e2b98e6b1602b900290ad8d2fc2130f08b
ngrahovac <ninagrahovac.96@gmail.com>
2024-03-20 14:25:11 +0100

    Fix table height and stick footer to the bottom

--- src/Peer.WebClient/src/comps/table2/ApplicationsTable.js
+++ src/Peer.WebClient/src/comps/table2/ApplicationsTable.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const ApplicationsTable = ({ children }) => {
     return (
-        <table className='w-full table-fixed text-left'>
-            {children}
-        </table>
+        <div className='h-96 relative'>
+            <table className='w-full table-fixed text-left'>
+                {children}
+            </table>
+        </div>
     )
 }
 
 ApplicationsTable.Header = ({ children }) => <thead><tr className='border-b-2 border-gray-200'>{children}</tr></thead>
-ApplicationsTable.Header.Cell = ({ children }) => <th className='font-medium truncate text-sm p-4 text-gray-400 uppercase text-left'>{children}</th>
+ApplicationsTable.Header.Cell = ({ children }) => <th className='font-medium truncate text-sm px-4 py-2 text-gray-400 uppercase text-left'>{children}</th>
 
 ApplicationsTable.Body = ({ children }) => <tbody>{children}</tbody>
 ApplicationsTable.Body.Row = ({ children, selected = false }) => <tr className={`${selected ? 'hover:bg-indigo-400 bg-indigo-300' : 'hover:bg-gray-200 bg:gray-100'}`}>{children}</tr>
@@ -19,9 +21,9 @@ ApplicationsTable.Footer = ({ children }) => <tfoot className='border-t-2 border
     {children}
 </tfoot>
 
-ApplicationsTable.Footer.Row = ({ children }) => <tr>{children}</tr>
+ApplicationsTable.Footer.Row = ({ children }) => <tr className='absolute bottom-4 w-full flex justify-between align-middle items-center place-items-center h-8'>{children}</tr>
 
-ApplicationsTable.Footer.Cell = ({ children, collspan = 1 }) => <td colSpan={collspan} className='text-left p-4 h-12 uppercase tracking-wider text-slate-400 text-sm font-medium'>
+ApplicationsTable.Footer.Cell = ({ children, collspan = 1 }) => <td colSpan={collspan} className='text-left px-4 py-1 h-8 uppercase tracking-wider text-slate-400 text-sm font-medium flex content-center'>
     {children}
 </td>
 
